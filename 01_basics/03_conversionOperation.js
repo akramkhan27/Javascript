c5e02f4556bda8f3633e77aec37de23e99375224
Akram khan <117516430+akramkhan52@users.noreply.github.com>
2023-12-10 12:18:49 +0000

    comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,5 @@
+// ---------------------------Conversions----------------------
+
 // let score ="33";
 // console.log(typeof score);
 // console.log(typeof (score));
@@ -77,8 +79,54 @@
 // let BoolIsLoggedIn=Boolean(isLoggedIn);
 // console.log(BoolIsLoggedIn);
 
-let someNumber=33;
-console.log(typeof someNumber);
-let numberInStr=String(someNumber);
-console.log(numberInStr);
-console.log(typeof numberInStr);
+// let someNumber=33;
+// console.log(typeof someNumber);
+// let numberInStr=String(someNumber);
+// console.log(numberInStr);
+// console.log(typeof numberInStr);
+
+// ---------------------------Operations----------------------
+
+// let value=3;
+// let negValue=-value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(2/6);
+// console.log(2%4);
+
+// let str1="Akram";
+// let str2=" Khan";
+// let str3=str1+str2;
+// console.log(str3);
+
+// console.log(1+2);
+// console.log("1"+2);
+// console.log(2+"1");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(((2+2)*3-2)/6);
+
+// console.log(+true);
+// console.log(+false);
+// console.log(+"");
+
+// let num1,num2,num3;
+// num1=num2=num3=2+2;
+
+let gameCounter=100;
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
