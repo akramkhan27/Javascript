cf752418b100957fe42d1edeb40a834ef0352b84
Akram khan <117516430+akramkhan52@users.noreply.github.com>
2023-12-26 10:39:08 +0000

    string, math and num

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -1,5 +1,47 @@
-const myName="Akram";
-const repocount=10;
+let myName="Akram";
+let repocount=10;
 // console.log(myName+repocount+" Hello");
 
-console.log("Hello, My name is ${myName} and my repocount is ${repocount}");
+console.log(`Hello, My name is ${myName} and my repocount is ${repocount}`);
+
+const name=new String("Akram khan");
+console.log(name[0]);
+console.log(name.__proto__);
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.toLocaleUpperCase());
+console.log(name.toLocaleLowerCase());
+console.log(name.indexOf('a'));
+console.log(name.lastIndexOf('a'));
+console.log(name.charAt(6));
+
+const newString=name.substring(0,4);
+console.log(newString);
+
+const anotherString=name.slice(-4,7);  //-tive value can be given in slice if we put it on substring then always start from 0 
+console.log(anotherString);
+
+const value="    akram    ";
+console.log(value);
+console.log(value.trim());
+
+url="hhtps://akram.com/akram%20khan";
+console.log(url.replace("%20","-"));
+console.log(url.replace("a","-"));
+console.log(url.replaceAll("a","-"));
+console.log(url.includes("marka"));
+console.log(url.includes("akram"));
+
+const unknownString="akram-khan-marka";
+const knownString="Akram Mustaq Khan";
+console.log(unknownString.split("-",2));
+console.log(knownString.split(" ",1));
+console.log(knownString.split(" "));
+console.log(unknownString);
+
+console.log(unknownString.concat(knownString));
+console.log(unknownString.endsWith("rka"));
+console.log(unknownString.endsWith("rk"));
+console.log(unknownString.startsWith("akram"));
+console.log(unknownString.startsWith("kram"));
---
+++ 01_basics/07_nums_and_math.js
@@ -0,0 +1,46 @@
+const score=400;
+// console.log(score);
+
+const value= new Number(100);
+// console.log(value);
+
+// console.log(value.toString().length);
+// console.log(value.toFixed(2));
+
+const otherNumber=123.6564;
+// console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toPrecision(3));
+// console.log(otherNumber.toPrecision(2));
+
+const hundred=1000000;
+// console.log(hundred.toLocaleString());
+// console.log(hundred.toLocaleString("en-IN"));
+
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MIN_SAFE_INTEGER);
+
+//  ******************Math***************
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.abs(4));
+// console.log(Math.round(3.4));
+// console.log(Math.round(3.6));
+// console.log(Math.ceil(2.1));
+// console.log(Math.floor(2.9));
+// console.log(Math.min(2,1,5,6));
+// console.log(Math.max(2,1,5,6));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log((Math.random()*10) + 1);
+console.log(Math.floor((Math.random()*10) + 1));
+
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+
+
+
