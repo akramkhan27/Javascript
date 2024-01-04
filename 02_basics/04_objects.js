7012c208c079c46b97c5dc546832033c3d3b7c70
Akram khan <117516430+akramkhan52@users.noreply.github.com>
2024-01-04 06:18:49 +0000

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -17,7 +17,55 @@ const regularUser={
         }
     }
 }
-console.log(regularUser.fullname.userfullname.firstname);
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1={1: 'A', 2: 'B'};
+const obj2={3: 'C', 4: 'D'};
+const obj4={5: 'E', 6: 'F'};
+// const obj3={obj1,obj2};
+// console.log(obj3);
+
+// const obj3=Object.assign(obj1,obj2);
+// console.log(obj3);
+
+// const obj3=Object.assign({},obj1,obj2);
+// console.log(obj3);
+
+// const obj3=Object.assign({},obj1,obj2,obj4);
+// console.log(obj3);
+
+const obj3={...obj1,...obj2,...obj4};
+// console.log(obj3);
+
+// const User=[
+//     {
+//         id: "123",
+//         email: "akramkhan@gmail.com",
+//     },
+//     {
+//         id: "123",
+//         email: "akramkhan@gmail.com",
+//     },
+//     {
+//         id: "123",
+//         email: "akramkhan@gmail.com",
+//     },
+//     {
+//         id: "123",
+//         email: "akramkhan@gmail.com",
+//     }
+// ]
+
+// console.log(User[0]);
+// console.log(User[0].id);
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+console.log(tinderUser.hasOwnProperty('LoggedIn'));
 
 
 
