4cad9a43f3a4551a16407a01ddf18a7d004bbeb5
Akram khan <117516430+akramkhan52@users.noreply.github.com>
2024-01-13 04:45:30 +0000

    truthy and falsy

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,30 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+//     case value:
+        
+//         break;   
+
+//     default:
+//         break;
+// }
+
+const month="march";
+switch(month){
+    case "jan": 
+        console.log(`The month is Jan`);
+        break;    
+    case "feb": 
+        console.log(`The month is February`);
+        break;    
+    case "march": 
+        console.log(`The month is March`);
+        break; 
+    case "apr": 
+        console.log(`The month is April`);
+        break;    
+    default:
+        console.log("Invalid input");    
+}
+
---
+++ 04_control_flow/03_truthy_falsy.js
@@ -0,0 +1,43 @@
+// const userEmails=[]; // truthy
+// const userEmails=""; // falsy
+// const userEmails=" ";   // truthy
+// const userEmails={};   // truthy
+// if(userEmails){
+//     console.log("login with email");
+// }
+// else{
+//     console.log("You don't have email");
+// }
+
+// falsy values: 
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// truthy values:
+// "0", 'false', " ", [], {}, function(){}
+
+// if(userEmails.length ===0){
+//     console.log("array is empty");
+// }
+
+// const emptyObject={};
+// if(Object.keys(Object.keys(emptyObject)).length ===0){
+//     console.log("Object is empty");
+// }
+
+// Nullish coalescing operator (??) : Null undefined     // protect from null and undefined value
+
+let val1;
+val1=5 ?? 10;
+val1=5 ?? null;
+val1=null ?? 10;
+val1=undefined ?? 10;
+// val1=null ?? undefined ?? 2 ?? 3;
+// console.log(val1);
+
+
+// Ternary operator :  condition ? true:false
+
+const softy=20;
+softy<=13?console.log("softy price is less than  13"):console.log("softy price is greater than 13");;
+
+
